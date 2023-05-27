2c58e7335e97bb60c4c175f55cfecfacde207634
baki <55778040+heewon00@users.noreply.github.com>
2023-05-27 16:52:32 +0900

    [level 3] Title: 베스트앨범, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -1,24 +1,18 @@
 def solution(genres, plays):
-    playlist=[] #정렬
-    genre_dic=dict() #총 재생 횟수
+    genre_dic=dict() #'장르' : [장르의 재생 횟수, [(음악 재생 횟수, 고유번호),...,]]
     
     for i in range(len(genres)):
         if genres[i] not in genre_dic:
-            genre_dic[genres[i]] = plays[i]
+            genre_dic[genres[i]] = [plays[i], [(plays[i],i)]]
         else:
-            genre_dic[genres[i]] += plays[i]
-        playlist.append((genres[i],plays[i],i))
+            genre_dic[genres[i]][0] += plays[i]
+            genre_dic[genres[i]][1].append((plays[i],i))
     
-    genre_dic = sorted(genre_dic.items(), key=lambda x:-x[1])
-    playlist.sort(key=lambda x:(-x[1], x[2]))
-    
-    result_dic=dict()
-    for genre, play in genre_dic:
-        result_dic[genre] = []
+    sort_genre = sorted(list(genre_dic.values()), key=lambda x:-x[0])
+    answer=[]
+    for genre_cnt, info in sort_genre:
+        info.sort(key=lambda x:(-x[0], x[1]))
+        for song_cnt, idx in info[:2]:
+            answer.append(idx)
         
-    for g,p,i in playlist:
-        result_dic[g].append(i)
-    
-    answer = sum([i[:2] for i in result_dic.values()],[])
-    
     return answer
